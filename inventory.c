45d10f5ffdaf03b9640004a294deab4ce949a9b2
“tiglot1” <moraispedro758@gmail.com>
2024-12-25 09:49:29 -0300

    finished I part

--- inventory.c
+++ inventory.c
@@ -50,3 +50,78 @@ int main(int argc, char *argv[])
         printf("\n")
     }
 }
+
+int find_parts(int number)
+{
+    for (int i = 0; i < num_parts; i++)
+        if (inventory[i].number == number)
+            return i;
+    return -1;
+}
+
+void insert()
+{
+    int part_number;
+    if (num_parts == MAX_PARTS)
+    {
+        printf("FULL DATABASE\n");
+        return;
+    }
+    printf("Enter part number: ");
+    scanf("%d", & part_number);
+
+    if (find_parts(part_number) >= 0)
+    {
+        printf("part already exists \n");
+        return;
+    }
+    inventory[num_parts].number = part_number;
+    printf("enter part name: ");
+    read_line(inventory[num_parts].number, NAME_LEN);
+    printf("Enter quantity on hand: ");
+    scanf("%d", &inventory[num_parts].on_hand);
+    num_parts ++;
+}
+
+void search()
+{
+    int i, number;
+    
+    printf("Enter part number: ");
+    scanf("%d", &number);
+    i = find_parts(number);
+
+    if ( i >= 0)
+    {
+        printf("Part number: %s\n", inventory[i].name);
+        printf("Quantity on hand: %d\n", inventory[i].on_hand);
+    }
+    else
+        printf("Part not found\n");
+}
+
+void update()
+{
+    int i, number, change;
+
+    printf("Enter part number: ");
+    scanf("%d", &number);
+
+    i = find_parts(number)
+    if( i >= 0)
+    {
+        printf("Enter quantity on hand: ");
+        scanf("%d", &change)
+        inventory[i].on_hand += change;
+    }
+    else
+        printf("part not found\n");
+}
+
+void print()
+{
+    int i;
+    printf("Part number   Part name   Quantity on hand\n");
+    for( i = 0; i < num_parts; i++)
+        printf("%7d       %-25s%11d\n", inventory[i].number, inventory[i].name, inventory[i].on_hand);
+}
